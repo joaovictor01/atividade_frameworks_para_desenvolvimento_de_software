b769035cfe94a9c9d5a257628cea1043fb413ce1
João Victor <joao_victor-321@hotmail.com>
2024-12-02 18:50:50 -0300

    update

--- flask/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- flask/docker_entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export FLASK_APP=app.py
-export FLASK_ENV=production
-uwsgi app.ini
